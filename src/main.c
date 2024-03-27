1c8d99d8aa7e8b278cb0367d5eb5a2823c301571
mathieu-schertenleib <schertenleib.mathieu@gmail.com>
2024-03-27 13:16:17 +0100

    Remove time-dependent and intensity render, nicer colors

--- src/main.c
+++ src/main.c
@@ -17,34 +17,25 @@ int main(void)
     const Shader shader =
         LoadShader(0, TextFormat("src/shader.frag", GLSL_VERSION));
 
-    const int screen_width_loc = GetShaderLocation(shader, "screen_width");
     const int screen_height_loc = GetShaderLocation(shader, "screen_height");
-    const int show_intensity_loc = GetShaderLocation(shader, "show_intensity");
     const int angle_loc = GetShaderLocation(shader, "angle");
-    const int time_loc = GetShaderLocation(shader, "time");
     const int num_sources_loc = GetShaderLocation(shader, "num_sources");
 
-    bool show_intensity = false;
     float angle = 0.0f;
     int num_sources = 4;
+    int num_sources_editing = false;
+
+    const int ui_width = 250;
 
     while (!WindowShouldClose())
     {
         const int screen_width = GetScreenWidth();
         const int screen_height = GetScreenHeight();
-        const float time = (float)GetTime();
+        const float ui_x = (float)(screen_width - ui_width);
 
         SetShaderValue(
-            shader, screen_width_loc, &screen_width, SHADER_UNIFORM_INT);
-        SetShaderValue(
             shader, screen_height_loc, &screen_height, SHADER_UNIFORM_INT);
-        const int show_intensity_int = (int)show_intensity;
-        SetShaderValue(shader,
-                       show_intensity_loc,
-                       &show_intensity_int,
-                       SHADER_UNIFORM_INT);
         SetShaderValue(shader, angle_loc, &angle, SHADER_UNIFORM_FLOAT);
-        SetShaderValue(shader, time_loc, &time, SHADER_UNIFORM_FLOAT);
         SetShaderValue(
             shader, num_sources_loc, &num_sources, SHADER_UNIFORM_INT);
 
@@ -52,23 +43,35 @@ int main(void)
         ClearBackground(BLACK);
 
         BeginShaderMode(shader);
-        DrawRectangle(0, 0, screen_width, screen_height, BLACK);
+        DrawRectangle(0, 0, screen_width - ui_width, screen_height, BLACK);
         EndShaderMode();
 
-        GuiCheckBox(
-            (Rectangle) {10, 50, 200, 50}, "Intensity", &show_intensity);
-        GuiSlider((Rectangle) {10, 100, 200, 50},
-                  NULL,
+        // TODO: Proper GUI with nice panel for the controls and a GUI-themed
+        // border around the simulation view
+        GuiSlider((Rectangle) {ui_x + 110, 10, 100, 20},
                   TextFormat("Angle: %5.1f", (double)angle),
+                  NULL,
                   &angle,
                   -90.0f,
                   90.0f);
-        GuiSpinner((Rectangle) {10, 150, 200, 50},
-                   "Sources",
-                   &num_sources,
-                   0,
-                   100,
-                   true);
+
+        if (GuiSpinner((Rectangle) {ui_x + 110, 40, 100, 20},
+                       "Sources",
+                       &num_sources,
+                       1,
+                       100,
+                       num_sources_editing))
+        {
+            num_sources_editing = !num_sources_editing;
+        }
+        if (num_sources < 1)
+        {
+            num_sources = 1;
+        }
+        else if (num_sources > 100)
+        {
+            num_sources = 100;
+        }
 
         DrawFPS(10, 10);
         EndDrawing();
--- src/shader.frag
+++ src/shader.frag
@@ -2,44 +2,31 @@
 
 out vec4 color;
 
-uniform int screen_width;
 uniform int screen_height;
-uniform int show_intensity;
 uniform float angle;
-uniform float time;
 uniform int num_sources;
 
 void main()
 {
     const float k = 200.0;
     const float omega = 10.0;
-    const float amplitude = 0.1;
+    const float amplitude = 0.2;
 
-    vec2 uv = gl_FragCoord.xy / screen_height;
+    vec2 xy = gl_FragCoord.xy / screen_height;
 
     float amplitude_real = 0.0;
     float amplitude_imag = 0.0;
     for (int i = 0; i < num_sources; ++i)
     {
-        float source_y = (num_sources == 1) ? 0.5 : (0.3 + float(i) / float(num_sources - 1) * 0.4);
-        float r = distance(vec2(0.1, source_y), uv);
-        float phase_shift = 8.0 * float(i) / float(num_sources - 1);
+        float source_y = (num_sources == 1) ? 0.5 : (0.35 + float(i) / float(num_sources - 1) * 0.3);
+        float r = distance(vec2(0.1, source_y), xy);
+        float phase_shift = (num_sources == 1) ? 0.0 : 8.0 * float(i) / float(num_sources - 1);
 
-        amplitude_real += amplitude / r * cos(-k * r + omega * time + phase_shift);
-        amplitude_imag += amplitude / r * sin(-k * r + omega * time + phase_shift);
+        amplitude_real += amplitude / r * cos(-k * r + phase_shift);
+        amplitude_imag += amplitude / r * sin(-k * r + phase_shift);
     }
 
-    if (show_intensity == 1)
-    {
-        float intensity = amplitude_real * amplitude_real + amplitude_imag * amplitude_imag;
-        intensity = clamp(10 * intensity / (float(num_sources) * float(num_sources)), 0.0, 1.0);
-        color = vec4(intensity, 0.65 * intensity, 0.0, 1.0);
-    }
-    else
-    {
-        amplitude_real /= float(num_sources);
-        float wave_pos = clamp(amplitude_real, 0.0, 1.0);
-        float wave_neg = clamp(-amplitude_real, 0.0, 1.0);
-        color = vec4(wave_pos, 0.0, wave_neg, 1.0);
-    }
+    float wave_pos = sqrt(clamp(amplitude_real / float(num_sources), 0.0, 1.0));
+    float wave_neg = sqrt(clamp(-amplitude_real / float(num_sources), 0.0, 1.0));
+    color = vec4(wave_pos, 0.6 * wave_pos + 0.4 * wave_neg, wave_neg, 1.0);
 }
